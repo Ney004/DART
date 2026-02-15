1e5a5ab28a5fcd654f7911f0dc55343b2dc11dd9
Ney004 <neyluiswwe@gmail.com>
2026-02-14 20:06:53 -0500

    Bases

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,5 +1,50 @@
+import "dart:io";
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_basics.calculate()}!');
+  print("Ingrese un numero entre 1 y 12: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number < 1 || number > 12) {
+    print("Numero Invalido");
+  }
+
+  switch (number) {
+    case 1:
+      print("Enero");
+      break;
+    case 2:
+      print("Febrero");
+      break;
+    case 3:
+      print("Marzo");
+      break;
+    case 4:
+      print("Abril");
+      break;
+    case 5:
+      print("Mayo");
+      break;
+    case 6:
+      print("Junio");
+      break;
+    case 7:
+      print("Julio");
+      break;
+    case 8:
+      print("Agosto");
+      break;
+    case 9:
+      print("Septiembre");
+      break;
+    case 10:
+      print("Octubre");
+      break;
+    case 11:
+      print("Noviembre");
+      break;
+    case 12:
+      print("Diciembre");
+      break;
+  }
 }
